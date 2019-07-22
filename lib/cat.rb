8f043ceb4ff8d07e56e0725996ca5595a5d2c689
Paul Holmes <paulholmes1993@gmail.com>
2019-07-22 22:19:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(init_name, init_mood = "nervous")
+    @name = init_name
+    @mood = init_mood
+  end
 end
